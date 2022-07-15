320dc57df01d4c05564f3b9c6a8a6f42394693db
libin <libin@imdada.cn>
2022-07-15 19:20:44 +0800

    注解

--- leetcode/topics/977-有序数组的平方.py
+++ leetcode/topics/977-有序数组的平方.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# 解体思路:
+#   采用双指针解法
+#   起始位置和终止比较平方的大小, 大的存入新数组并移动指针, 小的停留原地
+
 def aa(nums):
     i, j, k, n = 0, len(nums)-1, len(nums)-1, len(nums)
     ans = [-1] * n
